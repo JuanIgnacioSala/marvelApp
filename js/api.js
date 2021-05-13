d5295f1bb4012eaf20e0045f8e83e8ef4060538e
JuanIgnacioSala <JuanIgnacioSala5@hotmail.com>
2021-05-13 15:26:39 -0300

    Header section

---
+++ js/api.js
@@ -0,0 +1,4 @@
+// url destino: https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=a6cbb63939c6bb41f89092331e8246b9&hash=4ad0b35656da2eddf174505d3d41b2c9
+const publicKey = "a6cbb63939c6bb41f89092331e8246b9";
+const hash = "4ad0b35656da2eddf174505d3d41b2c9";
+const apiAutentication = `ts=1&apikey=${publicKey}&hash=${hash}`;

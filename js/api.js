8775045704d0db4c2ca1a85ecb4ac0c923455f3e
Juan Ignacio Sala <Juanignaciosala5@hotmail.com>
2021-05-14 00:20:17 -0300

    ico / fetch / css

--- js/api.js
+++ js/api.js
@@ -1,4 +1,38 @@
 // url destino: https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=a6cbb63939c6bb41f89092331e8246b9&hash=4ad0b35656da2eddf174505d3d41b2c9
+// SEACH HERO 
+// let searchHero = '' //// nameStartsWith=${searchHero}&
+
+// GET ELEMENTS
+let cardhero = document.getElementById('herocard')
+    // GET HEROES
 const publicKey = "a6cbb63939c6bb41f89092331e8246b9";
 const hash = "4ad0b35656da2eddf174505d3d41b2c9";
 const apiAutentication = `ts=1&apikey=${publicKey}&hash=${hash}`;
+
+const getAllHeroes = (offset) => {
+    URL = `https://gateway.marvel.com:443/v1/public/characters?orderBy=name&offset=${offset}&${apiAutentication}`;
+    fetch(URL)
+        .then(response => response.json())
+        .then(data => {
+            data.data.results.forEach(e => {
+                displayHeroes(e)
+            });
+        })
+        .catch(err => console.log(err));
+};
+getAllHeroes(0);
+
+// DISPLAY HEROES
+const displayHeroes = (e) => {
+    const imageURL = `${e.thumbnail.path}.${e.thumbnail.extension}`;
+    const hero = `
+                <div class="col">
+                <div class="card bgred rounded-3 h-100">
+                    <img style="height:100%" src="${imageURL}" class="card-img-top" alt="${e.name}">
+                    <div class="card-body text-center">
+                        <h3 class="card-title">${e.name}</h3>
+                    </div>
+                </div>
+                </div>`;
+    cardhero.insertAdjacentHTML('beforeEnd', hero);
+};
---
+++ js/displayHeroes.js
@@ -0,0 +1,3 @@
+const displayHeroes = () => {
+    return console.log('hgastg');
+}

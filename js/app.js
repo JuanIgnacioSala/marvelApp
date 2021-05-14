ac33ff308afab889c3e15b99ae2dd5bc3a07452c
JuanIgnacioSala <JuanIgnacioSala5@hotmail.com>
2021-05-14 10:36:17 -0300

    app

--- js/app.js
+++ js/app.js
@@ -4,7 +4,11 @@
 
 // GET ELEMENTS
 let cardhero = document.getElementById('herocard')
-    // GET HEROES
+
+// SEARCH
+let search = document.getElementById('search-input');
+
+// GET HEROES
 const publicKey = "a6cbb63939c6bb41f89092331e8246b9";
 const hash = "4ad0b35656da2eddf174505d3d41b2c9";
 const apiAutentication = `ts=1&apikey=${publicKey}&hash=${hash}`;
@@ -35,4 +39,8 @@ const displayHeroes = (e) => {
                 </div>
                 </div>`;
     cardhero.insertAdjacentHTML('beforeEnd', hero);
+};
+
+const getHeroesByName = search => {
+    URLName = `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${search}&orderBy=name&offset=${offset}&${apiAutentication}`
 };
